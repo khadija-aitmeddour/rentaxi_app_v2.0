bc257ea0f4c9a633c059832f2dc8774140e1a94f
khadija-aitmeddour <khadidja.aitmeddour@se.univ-bejaia.dz>
2024-05-01 11:41:39 +0200

    click on map and set markers

--- screens/MapScreen.jsx
+++ screens/MapScreen.jsx
@@ -2,32 +2,69 @@ import { View, Text } from 'react-native'
 import React, { useEffect, useState} from 'react'
 import MapboxGL from '@rnmapbox/maps'
 
-MapboxGL.setAccessToken('pk.eyJ1Ijoia2FkYWl0bSIsImEiOiJjbHZnOWo3NHowNGRjMnFueTZqeWp3bXAwIn0.ZzAnPbGro7A3mF4o6Vphqw');
 
 const MapScreen = ({route}) => {
-  const {positionCoords, destinationCoords} = route.params;
   
+  
+  const {positionCoords, destinationCoords, myRoute} = route.params;
+  const [destinationPointer, setDestinationPointer] = useState(destinationCoords);
+  
+  const handleMapPress = (event) => {
+    setDestinationPointer(event.geometry.coordinates);
+};
+  const distance = myRoute ? parseFloat( myRoute.distance / 1000 ).toFixed(0) : 0; //get the distance if the route is not null, transform to km and round it 
+  
+  
+
   return (
     <View style={{flex: 1}}>
-      
-        <MapboxGL.MapView style={{flex: 1}} styleURL={MapboxGL.StyleURL.Street}>
+        <Text
+        style={{height:20}}>{distance}</Text>
+        <MapboxGL.MapView 
+        style={{flex: 1}} 
+        styleURL={MapboxGL.StyleURL.Street}
+        onPress={handleMapPress} 
+        >
           <MapboxGL.Camera
-            zoomLevel={5}
+            zoomLevel={10}
             centerCoordinate={positionCoords}
             animationMode="flyTo"
             animationDuration={2000}
           />
-
+          
+          {positionCoords && (
           <MapboxGL.PointAnnotation
             id="userLocation"
             coordinate={positionCoords}
             title="Your location"
-          />
+          /> 
+          )}
+          {destinationPointer && (
           <MapboxGL.PointAnnotation
             id="userLocation"
-            coordinate={destinationCoords}
-            title="Your location"
-          />
+            coordinate={destinationPointer}
+            title="Your destination"
+          /> 
+        )} 
+        {!positionCoords && !destinationCoords && (
+          <MapboxGL.PointAnnotation
+          id="default"
+          coordinate={[0,0]}
+          title="you need a ship here"
+        />
+        )}
+        {/* puts marker on map when clicked */}
+        {/* {clickedCoords && (
+        <MapboxGL.PointAnnotation
+          id='selected'
+          coordinate={clickedCoords}
+        />
+      )} */}
+         {myRoute && (
+        <MapboxGL.ShapeSource id="routeSource" shape={{ type: 'LineString', coordinates: myRoute.geometry.coordinates }}>
+          <MapboxGL.LineLayer id="routeFill" style={{ lineColor: '#3d85c6', lineWidth: 5 }} />
+        </MapboxGL.ShapeSource>
+        )}
         </MapboxGL.MapView>
       
     </View>
