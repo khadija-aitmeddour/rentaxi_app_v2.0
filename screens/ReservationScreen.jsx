8c87c4191687407a4b83c72fa2e20523bffa911e
khadija-aitmeddour <khadidja.aitmeddour@se.univ-bejaia.dz>
2024-05-01 16:17:54 +0200

    get Position and send position name to the location input screen

--- screens/ReservationScreen.jsx
+++ screens/ReservationScreen.jsx
@@ -3,23 +3,49 @@ import React, { useEffect, useState} from 'react'
 import MapboxGL from '@rnmapbox/maps'
 import { useNavigation } from '@react-navigation/native'
 import Geolocation from '@react-native-community/geolocation'
+import { MAPBOX_ACCESS_TOKEN } from '../mapboxConfig'
 
 const ReservationScreen = () => {
   
   const navigation = useNavigation();
-  const [userLocation, setUserLocation] = useState(null);
-
-  useEffect(() => {
-    // Get user's current location
-    Geolocation.getCurrentPosition(
+  const [userPosition, setuserPosition] = useState(null);
+  const [userPositionName, setUserPositionName] = useState('');
+  
+  const getuserPosition = () => {
+      Geolocation.getCurrentPosition(
       position => {
         const { latitude, longitude } = position.coords;
-        setUserLocation([longitude, latitude]);
+        setuserPosition([longitude, latitude]);
       },
       error => console.error(error.message),
       { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
     );
-  }, []);
+  };
+  useEffect(() => {
+     getuserPosition();
+  }, [userPosition]);
+  
+  
+  const getPlaceName = async (position) => {
+    const apiUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${position[0]},${position[1]}
+    .json?access_token=${MAPBOX_ACCESS_TOKEN}`;
+    try {
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+      if (response.ok) {
+        setUserPositionName(data.features[0].place_name);
+        console.log(userPositionName)
+      } else {
+        return 'Place not found';
+      }
+    } catch (error) {
+      console.error('Error fetching place name:', error.message);
+      return null;
+    }
+  }
+  useEffect(() => {
+    getPlaceName(userPosition)
+  },[]);
 
   return (
     <View style={{flex: 1}}>
@@ -29,14 +55,14 @@ const ReservationScreen = () => {
         >
           <MapboxGL.Camera
             zoomLevel={8}
-            centerCoordinate={userLocation}
+            centerCoordinate={userPosition}
             animationMode="flyTo"
             animationDuration={2000}
           />
         
           <MapboxGL.PointAnnotation
-            id="userLocation"
-            coordinate={userLocation}
+            id="userPosition"
+            coordinate={userPosition}
             title="Your location"
           /> 
         </MapboxGL.MapView>
@@ -45,7 +71,7 @@ const ReservationScreen = () => {
         <View style={styles.panel}>
         <Text style={styles.panelText}>Lost? Let's find your way!</Text>
         <Text style={{paddingLeft:10, color: "#333"}}>Click below to choose your destination 🗺️</Text>
-        <TouchableOpacity style={styles.button} onPress={()=>{navigation.navigate('Location')}}>
+        <TouchableOpacity style={styles.button} onPress={()=>{navigation.navigate('Location', {userPositionName})}}>
           <Text style={styles.buttonText}>Destination</Text>
           <Image
             source={require('../arrow.png')}
@@ -98,7 +124,7 @@ const styles = StyleSheet.create({
     marginTop: 15,
   },
   buttonText: {
-    fontSize: 18,
+    fontSize: 17,
     color: '#333',
   },
   
